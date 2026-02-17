f8f1a9a6db90f2fdfdc189ef7941383c2102db41
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-17 19:08:08 +0100

    second milestone

--- main.js
+++ main.js
@@ -2,18 +2,20 @@ const kmPrice = 0.21;
 const seniorDiscount = 40;
 const youngDiscount = 20;
 const btn = document.getElementById('calculate-btn');
+const recapSection = document.getElementById('recap')
 
 function calcolaBiglietto() {
-    
+    event.preventDefault() /* prevent page refresh */
     const age= document.getElementById('age').value
     const km = document.getElementById('km').value
+    const name = document.getElementById('name').value
     let prezzoBase = km * kmPrice;
     let scontoApplicato = 0;
 
 
-    if (age < 18) {
+    if (age === 'Giovane') {
         scontoApplicato = youngDiscount;
-    } else if (age >= 65) {
+    } else if (age === 'Anziano') {
         scontoApplicato = seniorDiscount;
     }
 
@@ -21,6 +23,14 @@ function calcolaBiglietto() {
     const risparmio = (prezzoBase * scontoApplicato) / 100;
     const prezzoFinale = prezzoBase - risparmio;
     console.log(prezzoFinale)
+    recapSection.innerHTML = ` 
+        <p class='p-2'>Nome: ${name}</p>
+        <p class='p-2'>Km: ${km}</p>
+        <p class='p-2'>Età: ${age}</p>
+        <p class='p-2'><strong>Prezzo Finale: €${prezzoFinale.toFixed(2)}</strong></p>
+    `/* add recap to the page */
+    const title = document.getElementById('title')
+    title.classList.remove('d-none')
     return prezzoFinale
 }
 btn.addEventListener('click', calcolaBiglietto)
